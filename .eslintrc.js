e02e9a7fc99640d85149e0405e620e1dd9086797
Alexander Skachko <alexander.skachko@productmadness.com>
2021-06-30 10:14:55 +0300

    Add app template

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'node': true,
+    'es6': true,
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ],
+  'parser': 'babel-eslint',
+  'parserOptions': {
+    'ecmaVersion': 9,
+    'sourceType': 'module',
+    'ecmaFeatures': {
+      'jsx': true,
+    },
+  },
+  'plugins': ['react'],
+  'rules': {
+    'indent': ['error', 2],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'max-len': ['error', { 'code': 120 }],
+    'comma-dangle': ['error', 'always-multiline'],
+    'no-console': ['error', { 'allow': ['warn', 'error'] }],
+  },
+  'settings': {
+    'react': {
+      'version': 'detect',
+    },
+  },
+};
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new Dotenv(),
+  ],
+};
