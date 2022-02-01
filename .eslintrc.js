dd934ce93721d5e6b0ee437a1de21982305fe1e2
lucerion <alexander.skachko@gmail.com>
2022-02-01 11:05:14 +0300

    Fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
   ],
-  'parser': 'babel-eslint',
+  'parser': '@babel/eslint-parser',
   'parserOptions': {
     'ecmaVersion': 9,
     'sourceType': 'module',
